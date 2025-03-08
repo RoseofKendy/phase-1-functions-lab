f8dea4458c75a3a6c16e3d28a231f8f04cd1e795
Sharon Kendi <sharonkendi35@gmail.com>
2025-03-08 09:52:22 +0300

    Learns about functions math wise

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hq = 42;
+    return Math.abs(pickupLocation - hq);
+  }
+  
+  function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0; // Free sample
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02; // 2 cents per foot after 400 feet
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25; // Flat rate of $25
+    } else {
+      return "cannot travel that far";
+    }
+  }
